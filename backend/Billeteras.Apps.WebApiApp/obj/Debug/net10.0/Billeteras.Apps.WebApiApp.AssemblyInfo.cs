ab3b175509f9c11821f9319492939854f5ba9a00
fabrithompson <fabriciothompson2015@hotmail.com>
2026-06-24 22:47:22 -0300

    Corrección (frontend): actualización de BASE_URL para pruebas móviles y configuración de lanzamiento en backend
    - Se modificó BASE_URL en client.ts para usar la IP de la LAN para las pruebas de Expo Go.
    
    - Se actualizó launchSettings.json para usar '0.0.0.0' como applicationUrl y permitir el acceso externo.
    
    - Se modificó la cadena de conexión en appsettings.Development.json para que apunte a localhost\SQLEXPRESS para SQL Server.

--- backend/Billeteras.Apps.WebApiApp/obj/Debug/net10.0/Billeteras.Apps.WebApiApp.AssemblyInfo.cs
+++ backend/Billeteras.Apps.WebApiApp/obj/Debug/net10.0/Billeteras.Apps.WebApiApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Billeteras.Apps.WebApiApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49824ae5708a6c33cb9b2c65f6bb222d45489a8b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+256b5961ae27921470777b965c82e89a25fe7960")]
 [assembly: System.Reflection.AssemblyProductAttribute("Billeteras.Apps.WebApiApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Billeteras.Apps.WebApiApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
